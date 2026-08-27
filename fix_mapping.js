58131fe1557e9b10037880cb704da1a891395e31
ayushcody <ayushfeb@rediffmail.com>
2026-08-27 21:13:08 +0530

    Remove leftover script files

--- fix_mapping.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/components/MappingScreen.tsx', 'utf8');
-code = code.replace(/<\/div>\s*<\/div>\s*<\/div>\s*<\/div>\s*<\/div>\s*\);\s*\}/, '</div>\n      </div>\n    </div>\n  </div>\n);\n}');
-fs.writeFileSync('src/components/MappingScreen.tsx', code);
